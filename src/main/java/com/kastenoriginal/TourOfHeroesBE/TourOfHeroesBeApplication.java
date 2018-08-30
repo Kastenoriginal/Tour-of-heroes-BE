931ea6d7af25909162aaf936e8d2c96aa25a23a1
Patrik Varga <patoevoix@gmail.com>
2018-08-30 10:12:50 +0200

    Basic project created

---
+++ src/main/java/com/kastenoriginal/TourOfHeroesBE/TourOfHeroesBeApplication.java
@@ -0,0 +1,12 @@
+package com.kastenoriginal.TourOfHeroesBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOfHeroesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourOfHeroesBeApplication.class, args);
+	}
+}
